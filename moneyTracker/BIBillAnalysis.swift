d7c2b4aa652d506aff2e87db9d5ff16b50af1ec0
Silver <user@Users-MacBook-Air.local>
2015-06-09 21:02:51 +1000

    da

--- moneyTracker/BIBillAnalysis.swift
+++ moneyTracker/BIBillAnalysis.swift
@@ -10,31 +10,128 @@ import Foundation
 import Foundation
 class BIBillAnalysis: NSObject {
     // analyze Bill based on Income and Expense
-    var usedCategories:[String] = []
-    var expAmtByUsedCat:[Double] = []
-    var incAmtByUsedCat:[Double] = []
+    var expCatSix:[String] = []
+    var incCatSix:[String] = []
+    var expSix:[Double] = []
+    var incSix:[Double] = []
+    
+    //internal variables
+    private var sortOrder:Array<Bool> = []
+    
+    private var expUsedCategories:[String] = []
+    private var expAmtByUsedCat:[Double] = []
+    private var incAmtByUsedCat:[Double] = []
+    
+    private var expAmtPercentage:[Double] = []
+    private var incAmtPercentage:[Double] = []
+    
     convenience init(year:Int,month:Int) {
         self.init()
         let catSQL:String = "SELECT Category FROM Expense WHERE YearOfExpense = \(year) AND MonthOfExpense = \(month)"
         let catsRepeat:[String] = BIQuery(UTF8StringQuery: catSQL).resultsOfQuery()
         for catR in catsRepeat {
-            if !self.usedCategories.hasItem(catR, sameItem: catR) {
-                self.usedCategories.append(catR)
+            if !self.expUsedCategories.hasItem(catR, sameItem: catR) {
+                self.expUsedCategories.append(catR)
             }
         }
-        for catS in self.usedCategories {
+        for catS in self.expUsedCategories {
             let expSQL:String = "SELECT SUM(Amounts) FROM Expense WHERE YearOfExpense = \(year) AND MonthOfExpense = \(month) AND Category = '\(catS)'"
             self.expAmtByUsedCat.append(BIQuery(UTF8StringQuery: expSQL).resultOfQuery() ?? 0.0)
         }
-        for catS in self.usedCategories {
+        for catS in self.expUsedCategories {
             let incSQL:String = "SELECT SUM(Amounts) FROM Income WHERE YearOfExpense = \(year) AND MonthOfExpense = \(month) AND Category = '\(catS)'"
             self.incAmtByUsedCat.append(BIQuery(UTF8StringQuery: incSQL).resultOfQuery() ?? 0.0)
         }
+        
+        sortCatsAndAmt()
+        self.expAmtPercentage = amtToPercentage(self.expAmtByUsedCat)
+        self.incAmtPercentage = amtToPercentage(self.incAmtByUsedCat)
+        makeExpSix()
+        makeIncSix()
     }
-    
-    
-    
-    
+    private func amtToPercentage(amtAry:[Double]) -> [Double]{
+        var sum:Double = 0
+        var perAry:[Double] = []
+        for amt in amtAry {
+            sum += amt
+        }
+        for i in 1...amtAry.count{
+            let per = amtAry[i-1]/sum
+            perAry.append(per)
+        }
+        return perAry
+    }
+    private func sortCatsAndAmt(){
+        sort(&self.expAmtByUsedCat, { [unowned self](a,b) in
+            let bool:Bool = a > b
+            self.sortOrder.append(bool)
+            return bool
+            })
+        var i = 0
+        sort(&self.expUsedCategories, { [unowned self] (_,_) in
+            i++
+            return self.sortOrder[i-1]
+            })
+        //        println(self.usedCategories)
+        //        println(self.expAmtByUsedCat)
+    }
+    private func makeExpSix(){
+        if self.expUsedCategories.count == 6{
+            for i in 0...5{
+                self.expSix.append(self.expAmtPercentage[i])
+                self.expCatSix.append(self.expUsedCategories[i])
+            }
+        }else if self.expUsedCategories.count > 6 {
+            for i in 0...4{
+                self.expSix.append(self.expAmtPercentage[i])
+                self.expCatSix.append(self.expUsedCategories[i])
+            }
+            var sumAfterSix:Double = 0
+            for i in 5...self.expUsedCategories.count-1 {
+                sumAfterSix += self.expAmtPercentage[i]
+            }
+            self.expSix.append(sumAfterSix)
+            self.expCatSix.append("Others")
+        }else{
+            for i in 0...self.expUsedCategories.count-1 {
+                self.expSix.append(self.expAmtPercentage[i])
+                self.expCatSix.append(self.expUsedCategories[i])
+            }
+            for i in (self.expUsedCategories.count)...5{
+                self.expSix.append(0)
+                self.expCatSix.append("Others")
+            }
+        }
+    }
+    //Category Error: Need to update to IncCategory
+    private func makeIncSix(){
+        if self.expUsedCategories.count == 6{
+            for i in 0...5{
+                self.incSix.append(self.incAmtPercentage[i])
+                self.incCatSix.append(self.expUsedCategories[i])
+            }
+        }else if self.expUsedCategories.count > 6 {
+            for i in 0...4{
+                self.incSix.append(self.incAmtPercentage[i])
+                self.incCatSix.append(self.expUsedCategories[i])
+            }
+            var sumAfterSix:Double = 0
+            for i in 5...self.expUsedCategories.count-1 {
+                sumAfterSix += self.incAmtPercentage[i]
+            }
+            self.incSix.append(sumAfterSix)
+            self.incCatSix.append("Others")
+        }else{
+            for i in 0...self.expUsedCategories.count-1 {
+                self.incSix.append(self.incAmtPercentage[i])
+                self.incCatSix.append(self.expUsedCategories[i])
+            }
+            for i in (self.expUsedCategories.count)...5{
+                self.incSix.append(0)
+                self.incCatSix.append("Others")
+            }
+        }
+    }
     
     
     
